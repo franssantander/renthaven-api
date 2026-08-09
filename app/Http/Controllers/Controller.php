4e2d680b3dd3ebdfac6c6f3cc309c1d002324bc4
franssantander <santanderfrancisbeam@gmail.com>
2026-08-09 12:42:25 +0800

    refactor: clean up unused imports and comments in Controller; update laravel/sanctum version in composer.lock

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Support\ApiResponder;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\App;
-use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 abstract class Controller
@@ -21,9 +16,6 @@ abstract class Controller
         return ApiResponder::success($data, $message, $status);
     }
 
-    /**
-     * Return a standardized error JSON response.
-     */
     protected function error(
         ?Throwable $exception = null,
         ?string $message = null,
